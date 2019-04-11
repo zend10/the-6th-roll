b6cc4c4842594611c1241468b4d4583e0a6f3bee
Zen Dharma <zendharma10@gmail.com>
2019-04-12 02:19:12 +0700

    Add base

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+
+const discord = require('discord.js')
+const client = new discord.Client()
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!')
+  }
+})
+
+client.login(process.env.BOT_TOKEN)
